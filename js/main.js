ed20674c4d687402806e9281908e73d10b7d3c3a
nobuhiro.mishima <nobuhiro.mishima@outlook.com>
2024-07-04 00:05:56 +0900

    fix ver1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,14 @@
-const menu = document.querySelector('.nav-menu-sp');
-const btn = document.querySelector('.menu-sp');
+const menuTop = document.querySelector('.nav-menu-sp-top');
+const menuBtm = document.querySelector('.nav-menu-sp-btm');
+const btnTop = document.querySelector('.menu-sp-top');
+const btnBtm = document.querySelector('.menu-sp-btm');
 
-const menuOpen = function(){
-    menu.classList.toggle('menu-open');
+const menuOpenTop = function(){
+    menuTop.classList.toggle('menu-open');
+}
+const menuOpenBtm = function(){
+    menuBtm.classList.toggle('menu-open');
 }
 
-btn.addEventListener('click', menuOpen);
+btnTop.addEventListener('click', menuOpenTop);
+btnBtm.addEventListener('click', menuOpenBtm);
